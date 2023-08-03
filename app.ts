b67428fa110a38e7892373a4d2bd39b608dd57f7
Lin <47834369+lin-hikari@users.noreply.github.com>
2023-08-03 12:09:04 -0300

    Renamed routes file for more simplicity
    Since it's not for "tests" anymore

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak@v12.6.0/mod.ts";
 import "https://deno.land/std@0.196.0/dotenv/load.ts"; //loads env vars
 
-import testRoutes from './routes/testRoutes.ts';
+import testRoutes from './routes/routes.ts';
 
 const app = new Application();
 
